c3be00b6ea0d12edaa185e735c7840c7276b1b73
sydrob <55166840+sydrobhez@users.noreply.github.com>
2023-03-28 22:55:18 -0600

    updated JS

---
+++ script.js
@@ -0,0 +1,49 @@
+//When user clicks generate a random choice
+//Display choices
+//Show message about choices made by users
+//Compare computer vs user choice
+//Update the score
+
+const result = document.getElementById('result');
+
+let [comp_score, user_score] = [0,0];
+
+let gameRules = {
+    Rock: {
+        Rock: 'tie',
+        Scissors: 'lose',
+        Paper: 'win',
+        Spock: 'win',
+        Lizard: 'lose'
+    },
+    Scissors: {
+        Scissors: 'tie',
+        Rock: 'win',
+        Paper: 'lose',
+        Spock: 'win',
+        Lizard: 'lose'
+    },
+    Paper: {
+        Paper: 'tie',
+        Rock: 'lose',
+        Scissors: 'win',
+        Spock: 'lose',
+        Lizard: 'win'
+    },
+    Spock: {
+        Rock: 'lose',
+        Scissors: 'lose',
+        Paper: 'win',
+        Spock: 'tie',
+        Lizard: 'win'
+    },
+    Lizard: {
+        Rock: 'win',
+        Scissors: 'win',
+        Paper: 'lose',
+        Spock: 'lose',
+        Lizard: 'tie'
+    },
+}
+
+function
